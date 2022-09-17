cda2da415d46094457c10873576ed8a1429c06a7
Giomar L <giomarlimocaldas1234@gmail.com>
2022-09-17 16:35:36 -0500

    aplicando migraciones get y post sql server

--- back-end/Controllers/GenerosController.cs
+++ back-end/Controllers/GenerosController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,16 +18,19 @@ namespace back_end.Controllers
     public class GenerosController: ControllerBase
     {
         private readonly ILogger<GenerosController> logger;
+        private readonly ApplicationDbContext context;
 
-        public GenerosController(ILogger<GenerosController> logger)
+        public GenerosController(ILogger<GenerosController> logger,
+                                 ApplicationDbContext context)
         {
             this.logger = logger;
+            this.context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Genero>> Get() 
+        public async Task<ActionResult<List<Genero>>> Get() 
         {
-            return new List<Genero>() { new Genero() { id = 1, nombre = "Prueba desde back01" } };
+            return await context.Generos.ToListAsync();
         }
 
         [HttpGet("{id:int}")]
@@ -36,9 +40,11 @@ namespace back_end.Controllers
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] Genero genero) 
+        public async Task<ActionResult> Post([FromBody] Genero genero) 
         {
-            throw new NotImplementedException();
+            context.Add(genero);
+            await context.SaveChangesAsync();
+            return NoContent();
         }
 
         [HttpPut]
--- back-end/Startup.cs
+++ back-end/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +32,8 @@ namespace back_end
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
